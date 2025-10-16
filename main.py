591663e3500b57538c5b048c8e229b03338b797a
Abindaaa <abindacarmo08@gmail.com>
2025-10-17 01:33:50 +0700

    aumenta ona funsaun calculator sira

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 numeru1 = float(input("Hatama numeru1: "))
 operasaun = input("hili operasaun (+, -, *, /): ")
 numeru2 = float(input("Hatama numeru2: "))
+
+if operasaun == "+":
+    result = numeru1 + numeru2
+elif operasaun == "-":
+    result = numeru1 - numeru2
+elif operasaun == "*":
+    result = numeru1 * numeru2    
+else:
+    if numeru2 != 0:
+        result = numeru1 / numeru2
+    else: 
+        print("errorrrrrrr😒")
+
+print(numeru1, " ", operasaun, " ", numeru2, " = ", result)
